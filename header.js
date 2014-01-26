c3592eff14d189dce6fd1b4c8ceff3df128d7a7f
Harrison Ho <harrison8989@gmail.com>
2014-01-26 15:38:49 -0800

    Tab that is active is given the active class

--- header.js
+++ header.js
@@ -1,10 +1,49 @@
-document.write('     <nav class="navbar navbar-default" role="navigation">        <!-- Brand and toggle get grouped for better mobile display -->        <div class="navbar-header">          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">            <span class="sr-only">Toggle navigation</span>            <span class="icon-bar"></span>            <span class="icon-bar"></span>            <span class="icon-bar"></span>          </button>');
-document.write('<a class="navbar-brand" href="login.html">Code The Change</a>        </div>        <!-- Collect the nav links, forms, and other content for toggling -->        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">');
+document.write('<nav class="navbar navbar-default" role="navigation">');
+document.write('<!-- Brand and toggle get grouped for better mobile display -->');
+document.write('<div class="navbar-header">');
+document.write('<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">');
+document.write('<span class="sr-only">Toggle navigation</span>');
+document.write('<span class="icon-bar"></span>');
+document.write('<span class="icon-bar"></span>');
+document.write('<span class="icon-bar"></span></button>');
+document.write('<a class="navbar-brand" href="login.html">Code The Change</a>');
+document.write('</div>');
+
+document.write('<!-- Collect the nav links, forms, and other content for toggling -->');
+document.write('<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">');
 document.write('<ul class="nav navbar-nav">');
-document.write('<li><a href="Dashboard.html">Home</a></li>');
-document.write('<li><a href="non_profit_partner_profiles.html">Partners</a></li>');
-document.write('<li><a href="allProjects.html">Projects</a></li>');
-document.write('<li><a href="non_profit_partner_profiles.html">Non-Profits</a></li>');
-document.write('<li><a href="projectCreate.html">Create New Project</a></li>');
-document.write('<li class="dropdown">              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>              <ul class="dropdown-menu">                <li><a href="#">Action</a></li>                <li><a href="#">Another action</a></li>                <li><a href="#">Something else here</a></li>                <li class="divider"></li>                <li><a href="#">Separated link</a></li>                <li class="divider"></li>                <li><a href="#">One more separated link</a></li>              </ul>            </li>          </ul>');
-document.write('<form class="navbar-form navbar-right" role="search">            <div class="form-group">              <input type="text" class="form-control" placeholder="Search">            </div>            <button type="submit" class="btn btn-default">Submit</button>          </form>          <ul class="nav navbar-nav navbar-right">          </ul>        </div><!-- /.navbar-collapse -->      </nav>');
+
+var url = document.URL;
+var split = url.split("/");
+var page = split[split.length - 1];
+var pages = ["dashboard.html", "non_profit_partner_profiles.html", "allProjects.html", "projectCreate.html"];
+var titles = ["Home", "Partners", "Projects", "Create New Project"];
+
+for(var i = 0; i < 4; i++) {
+	document.write('<li');
+	if(page === pages[i]) {
+		document.write(' class="active"');
+	}
+	document.write('><a href="' + pages[i] + '">' + titles[i] + '</a></li>');
+	//<li><a href="pages[i]">titles[i]</a></li>
+}
+
+document.write('<li class="dropdown">');
+document.write('<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>');
+document.write('<ul class="dropdown-menu">');
+document.write('<li><a href="#">Action</a></li>');
+document.write('<li><a href="#">Another action</a></li>');
+document.write('<li><a href="#">Something else here</a></li>');
+document.write('<li class="divider"></li>');
+document.write('<li><a href="#">Separated link</a></li>');
+document.write('<li class="divider"></li>');
+document.write('<li><a href="#">One more separated link</a></li>');
+document.write('</ul></li></ul>');
+
+
+document.write('<form class="navbar-form navbar-right" role="search">');
+document.write('<div class="form-group">');
+document.write('<input type="text" class="form-control" placeholder="Search"></div>');
+document.write('<button type="submit" class="btn btn-default">Submit</button></form>');
+document.write('<ul class="nav navbar-nav navbar-right"></ul></div>');
+document.write('<!-- /.navbar-collapse --></nav>');
