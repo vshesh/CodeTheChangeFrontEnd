5996416b72cd7f9f36c2ce31130cf94595034f59
Vishesh Gupta <vishesh@stanford.edu>
2014-02-13 14:45:09 -0800

    initial working node app

--- footer.js
+++ footer.js
@@ -1,4 +1,5 @@
-document.write('<div class="container">')
-document.write('<hr />')
-document.write('<p align="center">Created by the <a href="developers.html">Developers</a> of Code the Change</p>')
-document.write('</div>')
+<div class="container">
+  <hr />
+  <p align="center">Created by the <a href="developers.html">Developers</a> of Code the Change</p>
+</div>
+
